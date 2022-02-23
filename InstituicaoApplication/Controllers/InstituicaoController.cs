601d548149ad2447eb996b2678ba9bb103d56ec0
Ariane Rocha <72419533+arianerfrancisco@users.noreply.github.com>
2022-02-23 09:20:05 -0300

    implementando views e controller

--- InstituicaoApplication/Controllers/InstituicaoController.cs
+++ InstituicaoApplication/Controllers/InstituicaoController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InstituicaoApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace InstituicaoApplication.Controllers;
 
 public class InstituicaoController : Controller
 {
-    // GET
+    // o campo static é para permitir ser compartilhador entre as requisições
+    private static IList<Instituicao> instituicoes =
+        new List<Instituicao>()
+        {
+            new Instituicao() {
+                InstituicaoID = 1,
+                Nome = "UniParaná",
+                Endereco = "Paraná"
+            },
+            new Instituicao() {
+                InstituicaoID = 2,
+                Nome = "UniSanta",
+                Endereco = "Santa Catarina"
+            },
+            new Instituicao() {
+                InstituicaoID = 3,
+                Nome = "UniSãoPaulo",
+                Endereco = "São Paulo"
+            },
+            new Instituicao() {
+                InstituicaoID = 4,
+                Nome = "UniSulgrandense",
+                Endereco = "Rio Grande do Sul"
+            },
+            new Instituicao() {
+                InstituicaoID = 5,
+                Nome = "UniCarioca",
+                Endereco = "Rio de Janeiro"
+            }
+        };
     public IActionResult Index()
     {
-        return View();
+        return View(instituicoes);
     }
 }
